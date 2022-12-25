2d416eb7f4e77b275aead2efc693081593d58f7c
Javier Oviedo <lucasjavoeroviedo@gmail.com>
2022-12-25 13:54:24 -0300

    Actualizacion OpenBootcamp

---
+++ OpenBootcamp/Desarrollo FullStack/Main.java
@@ -0,0 +1,21 @@
+// Funciones 
+
+public class Main {
+    public static void main(String[] args){
+        //int resultado;
+        //resultado = suma( 13, 48);
+
+        //System.out.println(resultado);
+        suma(16, 98);
+    }
+
+   // public static int suma(int a, int b){
+   //     return a + b;
+    public static void suma(int a, int b){
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a * b);
+    }    
+}
+// Toda la logica va dentro de la funcion, entre llaves
