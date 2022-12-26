f15af2e16bf36350a61493ef7e38b537b84fd7cb
Javier Oviedo <lucasjavoeroviedo@gmail.com>
2022-12-25 21:59:54 -0300

    Ejercicio 1

--- OpenBootcamp/Desarrollo FullStack/Main.java
+++ OpenBootcamp/Desarrollo FullStack/Main.java
@@ -1,21 +1,56 @@
 // Funciones 
 
-public class Main {
-    public static void main(String[] args){
+//public class Main {
+    //public static void main(String[] args){
         //int resultado;
         //resultado = suma( 13, 48);
 
         //System.out.println(resultado);
-        suma(16, 98);
-    }
+        //suma(16, 98);
+    //}
 
    // public static int suma(int a, int b){
    //     return a + b;
-    public static void suma(int a, int b){
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a / b);
-        System.out.println(a * b);
-    }    
-}
+    //public static void suma(int a, int b){
+       // System.out.println(a + b);
+       // System.out.println(a - b);
+       // System.out.println(a / b);
+       // System.out.println(a * b);
+    //}    
+//}
+
 // Toda la logica va dentro de la funcion, entre llaves
+
+//public class Main{
+
+//    public static void main(String[] args){
+  //      Potato miPotato = new Potato();
+//        miPotato.QuitarBrazo();
+  //      miPotato.QuitarBrazo();
+    //    System.out.println(miPotato.brazos);        
+//    }
+//}
+//class Potato{
+  //  public int brazos = 0;
+
+//    public void QuitarBrazo(){
+  //      this.brazos--;
+   // }
+//}
+
+// Ejercicio 1- Funciones Avanzadas
+
+public class Main{
+
+    public static void main(String[] args){
+        suma(13, 22, 47);
+        System.out.println();
+    }
+
+
+    public static void suma(int a, int b, int c){
+        System.out.println(a+b+c);
+        
+    }
+
+}
